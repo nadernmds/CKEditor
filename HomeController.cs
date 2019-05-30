65245053f888c286e79960b35c6b01096c8314a5
nadernmds <37673805+nadernmds@users.noreply.github.com>
2019-05-30 04:42:59 +0430

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication13.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult About(HttpPostedFileBase upload,string text)
+        {
+            upload.SaveAs(Server.MapPath("~/images/name.jpg"));
+            var obj = new { uploaded = 1, fileName = "pep", url = "/images/name.jpg" };
+            return Json(obj,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
